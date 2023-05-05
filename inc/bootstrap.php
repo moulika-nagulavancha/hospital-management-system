c2f59f872bf7ff6a1b44db56d616af7501ba708b
moulika-nagulavancha <moulika152@gmail.com>
2023-05-05 16:12:13 -0500

    Database Theory and Applications project checkin

---
+++ inc/bootstrap.php
@@ -0,0 +1,8 @@
+<script type="text/javascript" src="https://code.jquery.com/jquery-3.5.1.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.9.2/umd/popper.min.js"></script>
+
+<!-- 
+   <script src="/dbms/assets/js/jquery.js"></script>
+   <script src="/dbms/assets/js/bootstrap.min.js"></script>
+   <script src="/dbms/assets/js/popper.js"></script> -->
